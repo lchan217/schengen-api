b1d02cb06739d705442beb499b74eae6e60ec338
Lizzie <lchan217@gmail.com>
2020-04-21 09:24:08 -0700

    add auth_controller and add encode_token to users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   #user can't be authenticated if they don't exist
   skip_before_action :authorized, only: [:create]
+    
     def create
-        @user = User.create(user_params)
-        if @user.valid?
-          render json: { user: UserSerializer.new(@user) }, status: :created
-        else
-          render json: { error: 'failed to create user' }, status: :not_acceptable
-        end
+      @user = User.create(user_params)
+      if @user.valid?
+        @token = encode_token({ user_id: @user.id }) #from app_controller
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      else
+        render json: { error: 'failed to create user' }, status: :not_acceptable
+      end
     end
 
     private
