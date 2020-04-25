6bf127b3f6194bc83ebf59b6297d8c5eeca164e2
Lizzie <lchan217@gmail.com>
2020-04-25 11:31:16 -0700

    change username to pw

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_04_25_180558) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
