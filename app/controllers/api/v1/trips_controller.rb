8674f36675f9d0558c27f2fb226f57d89cb2c779
Lizzie <lchan217@gmail.com>
2020-05-08 10:41:58 -0700

    add strong params to trip

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::TripsController < ApplicationController
         @trips = current_user.trips
         render json: @trips, status: 201
     end
+
+    def trip_params
+        params.require(:trip).permit(:entry, :exit, :location, :passed, :user_id)
+    end
 end
