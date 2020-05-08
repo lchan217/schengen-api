060d6d6d5ba1a46e5bdd5c43ee3c10996993913a
Lizzie <lchan217@gmail.com>
2020-05-08 11:07:45 -0700

    add logic render past and future trips

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::TripsController < ApplicationController
     def index
-        @trips = current_user.trips
-        render json: @trips, status: 201
+        trips = current_user.trips
+        past = trips.select {|trip| trip.passed == true}
+        future = trips.select {|trip| trip.passed == false}
+        render json: {past: past, future: future}, status: 201
     end
+
+    def create 
+        # logic to assign passed attr
+    end 
 
     def trip_params
         params.require(:trip).permit(:entry, :exit, :location, :passed, :user_id)
