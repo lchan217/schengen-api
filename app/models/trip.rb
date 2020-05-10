0565d065e17c1ca0835f45a06b8267707a4cc995
Lizzie <lchan217@gmail.com>
2020-05-09 17:21:00 -0700

    add passed attr based on exit date

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,8 @@ class Trip < ApplicationRecord
     validates :entry, presence: true
     validates :exit, presence: true
     validates :location, presence: true
+
+    def assign_passed(trip)
+        trip.exit+1 > Date.current() ? trip.passed = 1 : trip.passed = 0
+    end
 end
