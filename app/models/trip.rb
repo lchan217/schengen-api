08d3637039c565893973493ae172153acac36035
Lizzie <lchan217@gmail.com>
2020-05-12 09:35:19 -0700

    minor refactor

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,8 +3,7 @@ class Trip < ApplicationRecord
     validates :entry, presence: true
     validates :exit, presence: true
     validates :location, presence: true
-    validate :exit_cannot_be_before_entry
-    validate :schengen_countries
+    validate :exit_cannot_be_before_entry, :schengen_countries
 
     def assign_attrs(trip)
         trip.exit+1 > Date.current() ? trip.passed = 0 : trip.passed = 1
@@ -15,14 +14,10 @@ class Trip < ApplicationRecord
     def schengen_countries
         countries = ["Austria", "Belgium", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Iceland", "Italy", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Norway", "Poland", "Portugal", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland"]
 
-        if !countries.include?(location)
-            errors.add(:location, "must be in the Schengen Area")
-        end
+        errors.add(:location, "must be in the Schengen Area") if !countries.include?(location)
     end 
 
     def exit_cannot_be_before_entry
-        if exit && exit+1 < entry 
-            errors.add(:exit, "Date of exit cannot be before date of entry")
-        end
+        errors.add(:exit, "Date of exit cannot be before date of entry") if exit && exit+1 < entry 
     end
 end
