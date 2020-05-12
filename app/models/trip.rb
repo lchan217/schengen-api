c845b8a7ef71393dd59c29bd3462d065e0cfda8c
Lizzie <lchan217@gmail.com>
2020-05-11 17:03:22 -0700

    add validation for entry/exit dates

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,9 +3,16 @@ class Trip < ApplicationRecord
     validates :entry, presence: true
     validates :exit, presence: true
     validates :location, presence: true
+    validate :exit_cannot_be_before_entry
 
     def assign_attrs(trip)
         trip.exit+1 > Date.current() ? trip.passed = 0 : trip.passed = 1
-        trip.time_passed = (trip.exit - trip.entry).to_i
+        trip.time_passed = (trip.exit+1 - trip.entry).to_i
+    end
+
+    def exit_cannot_be_before_entry
+        if exit+1 < entry 
+            errors.add(:exit, "Date of exit cannot be before date of entry")
+        end
     end
 end
